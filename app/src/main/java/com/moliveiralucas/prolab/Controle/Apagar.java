1e50a56fbe575f3d2943f89e82666652f4d24d92
MOliveiraLucas <lucasoliveiramarques@hotmail.com.br>
2017-12-08 13:43:41 -0200

    Inicio da implementação da classe Apagar.java

--- app/src/main/java/com/moliveiralucas/prolab/Controle/Apagar.java
+++ app/src/main/java/com/moliveiralucas/prolab/Controle/Apagar.java
@@ -3,11 +3,13 @@ package com.moliveiralucas.prolab.Controle;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
@@ -16,6 +18,7 @@ import com.moliveiralucas.prolab.R;
 
 public class Apagar extends Fragment {
     private String[] tipoCadastro = {"Selecione o assunto", "Exame", "Laboratório", "Exame de Laboratório"};
+    private Integer apagar;
 
     public Apagar() {
         // Required empty public constructor
@@ -36,8 +39,8 @@ public class Apagar extends Fragment {
                 LinearLayout apagarExame = v.findViewById(R.id.groupExcluirExame);
                 LinearLayout apagarLaboratorio = v.findViewById(R.id.groupExcluirLaboratorio);
                 LinearLayout apagarExameLab = v.findViewById(R.id.groupExcluirExameLab);
-                Integer apagar = (Integer) spinner.getSelectedItemPosition();
-                switch(apagar){
+                apagar = (Integer) spinner.getSelectedItemPosition();
+                switch (apagar) {
                     case 0:
                         apagarExame.setVisibility(View.GONE);
                         apagarExameLab.setVisibility(View.GONE);
@@ -66,7 +69,42 @@ public class Apagar extends Fragment {
 
             }
         });
+        Button btnEnviar = v.findViewById(R.id.btnEnviarDelete);
+        btnEnviar.setOnClickListener(enviar());
+        Button btnCancelar = v.findViewById(R.id.btnCancelarDelete);
+        btnCancelar.setOnClickListener(cancelar());
         return v;
     }
 
+    private View.OnClickListener enviar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (apagar) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        };
+    }
+
+    private View.OnClickListener cancelar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFragment(new Empety(), "Empety");
+            }
+        };
+    }
+
+    private void showFragment(Fragment fragment, String name) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.idContainer, fragment, name);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
